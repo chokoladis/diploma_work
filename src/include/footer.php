85649bc18910e6a6de58a0b64d9002d19a768017
chokoladis <igor2011__igor@mail.ru>
2026-02-11 23:58:13 +0400

    start rework 2026

---
+++ src/include/footer.php
@@ -0,0 +1,59 @@
+
+<div class="on_top">▲</div>
+<footer>
+    <div class="container">
+        <div class="padding-top w100 d-flex justify-content-center">
+            <div class="footer_item">
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">Адрес</h4>
+                <p class="animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">г. Бугуруслан, ул. Красногвардейская 41</p>
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">Телефоны</h4>
+                <p class="animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">
+                    +7 (932) 555-55-22<br>
+                    +7 (922) 855-55-54
+                </p>
+            </div>
+            <div class="footer_item">
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">Реквизиты</h4>
+                <p class="animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">ИНН 560200748211 <br>
+                    ОГРНИП 304560201700021</p>
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">График работы</h4>
+                <p class="animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">
+                    Открыто c 10:00 до 19:00<br>
+                    Выходной - сб, вс
+                </p>
+            </div>
+            <div class="footer_item">
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">Соцсети</h4>
+                <a href="https://vk.com/redmouse">
+                    <img src="../css/img/вк.png" alt="" class="animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;"></a>
+            </div>
+            <div class="map">
+                <h4 class="upper animated wow slideInLeft animated" style="visibility: visible; animation-name: slideInLeft;">Геолокация</h4>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1182.3369764952852!2d52.42806900723814!3d53.652785596262525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4164074b28662273%3A0xddf5b1f804070d41!2z0JrRgNCw0YHQvdC-0LPQstCw0YDQtNC10LnRgdC60LDRjyDRg9C7LiwgNDEsINCR0YPQs9GD0YDRg9GB0LvQsNC9LCDQntGA0LXQvdCx0YPRgNCz0YHQutCw0Y8g0L7QsdC7LiwgNDYxNjMw!5e0!3m2!1sru!2sru!4v1557843021007!5m2!1sru!2sru" class="map wow rollIn" frameborder="0" allowfullscreen="true"></iframe>
+            </div>
+        </div>
+        <div class="footer_block_a d-flex justify-content-center">
+            <div class="footer_a"><a href="../map.php">Карта сайта</a></div>
+            <div class="footer_a"><a href="../sp_info.php">Справочная информация</a></div>
+            <div class="footer_a"><a href="../ask.php">Вопросы и ответы</a></div>
+            <div class="footer_a"><a href="../calendar.php">Календарь</a></div>
+        </div>
+
+        <p class="copyright slideInDown wow animated">© «Цифровая студия RedMouse (ИП Соловьев)», 2019</p>
+        <div class="looks">
+            <a href="http://www.warlog.ru/" target="_blank"><img border="0" src="http://www.warlog.ru/counter/?i=3003" alt="счетчик посещений" title="счетчик посещений" /></a>
+        </div>
+    </div>
+</footer>
+
+<script src="../js/main.js"></script>
+<script src="../js/parallax-mouse.js"></script>
+<script src="../js/header.js"></script>
+<script src="../js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="../js/slider.js"></script>
+
+</body>
+</html>
---
+++ src/include/header.php
@@ -0,0 +1,17 @@
+<?
+    include '../db.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>redmouse.ru</title>
+
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles1.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
